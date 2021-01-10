35a15569452d5fa152702e5afe4ffdc4c852acd4
wgr4218 <william.roth@gmx.de>
2021-01-10 22:33:52 +0100

    Another commit DB

--- src/main/java/DBacc.java
+++ src/main/java/DBacc.java
@@ -1,23 +1,6 @@
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
 
 @WebServlet("/DB")
-public class DBacc extends HttpServlet {
-    int count = 0;
-   /*  GetDB db = new GetDB();
-    String brand = db.getBrand().get(0);*/
-/*    @Override
-    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException,
-            IOException {
-        resp.setContentType("text/html");
-        PrintWriter out = resp.getWriter();
-        out.println(1);
-        out.println(brand);
-    }
-    */
+public class DBacc {
+
 }
