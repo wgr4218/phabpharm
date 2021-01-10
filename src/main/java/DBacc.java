4fda36ca64df00215fb7b57297c5ac6e43a11025
wgr4218 <william.roth@gmx.de>
2021-01-10 22:46:51 +0100

    Another commit DB

--- src/main/java/DBacc.java
+++ src/main/java/DBacc.java
@@ -1,7 +1,13 @@
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @WebServlet("/DB")
 public class DBacc extends HttpServlet {
-
+    GetDB db= new GetDB();
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException,
+            IOException {}
 }
