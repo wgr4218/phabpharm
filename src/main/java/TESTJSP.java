91988f82c888f5a599e6f08254c2022bf63288f7
wgr4218 <william.roth@gmx.de>
2021-01-12 11:20:59 +0100

    JSP test

---
+++ src/main/java/TESTJSP.java
@@ -0,0 +1,16 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/DB")
+public class TESTJSP extends HttpServlet {
+
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException,
+            IOException {
+        req.getRequestDispatcher("../JSPs/test.jsp").include(req, resp);
+    }
+}
